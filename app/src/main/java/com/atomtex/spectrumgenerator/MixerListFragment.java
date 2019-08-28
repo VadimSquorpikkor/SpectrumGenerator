6f2b05d91606900c032ca27f7b08d8c93766b156
VadimSquorpikkor <uniqueehub1024>
2019-08-28 17:09:42 +0300

    work 40 ver. 1.03

--- app/src/main/java/com/atomtex/spectrumgenerator/MixerListFragment.java
+++ app/src/main/java/com/atomtex/spectrumgenerator/MixerListFragment.java
@@ -1,36 +1,25 @@
 package com.atomtex.spectrumgenerator;
 
-import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-
-import static com.atomtex.spectrumgenerator.MainActivity.TAG;
 
 public class MixerListFragment extends Fragment implements View.OnClickListener {
 
     private static MainViewModel mViewModel;
     View v;
     SourceAdapter sourceAdapter;
-    Button bt;
     ListView lvMain;
-    Toggler toggler;
-    Button hide_time;
 
     public static MixerListFragment newInstance() {
         return new MixerListFragment();
--- app/src/main/java/com/atomtex/spectrumgenerator/util/NuclideLibraryReader.java
+++ app/src/main/java/com/atomtex/spectrumgenerator/util/NuclideLibraryReader.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import com.atomtex.spectrumgenerator.domain.EnergyLine;
 import com.atomtex.spectrumgenerator.domain.Nuclide;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -16,11 +19,12 @@ import java.util.ArrayList;
 import java.util.List;
 import static com.atomtex.spectrumgenerator.MainActivity.TAG;
 import static com.atomtex.spectrumgenerator.domain.Nuclide.State.UNIDENTIFIED;
+import static com.atomtex.spectrumgenerator.domain.Nuclide.State.VALID;
 import static com.balsikandar.crashreporter.CrashReporter.getContext;
 
 public final class NuclideLibraryReader {
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
+//    @TargetApi(Build.VERSION_CODES.KITKAT)
     public static List<Nuclide> getLibrary(String path) throws IOException {
         Log.e(TAG, "попытка загрузки библиотеки по адресу: " + path);
         List<Nuclide> list = new ArrayList<>();
@@ -33,39 +37,60 @@ public final class NuclideLibraryReader {
         Uri uri = Uri.parse(path);
         InputStream inputStream = getContext().getContentResolver().openInputStream(uri);
 
+        //for Encrypter
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[16384];
+        InputStream inputByteStream = getContext().getContentResolver().openInputStream(uri);
+        while ((nRead = inputByteStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+
+        list = libraryParser(charsets, data);
+        if(list.size()==0) {
+            list = libraryParser(charsets, Encrypter.convertData(data));
+            Log.e(TAG, "**************ENCRYPTER STARTS************************");
+        }
+        return list;
+    }
+
+    private static List<Nuclide> libraryParser(List<Charset> charsets, byte[] data) throws IOException {
+        List<Nuclide> list = new ArrayList<>();
         for (Charset charset : charsets) {
             try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset));
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(data), charset));
 
-            String firstLine = reader.readLine();
-            int nuclNum = Integer.parseInt(firstLine);
-            for (int i = 0; i < nuclNum; i++) {
-                Nuclide nuclide = new Nuclide();
-                String[] string = reader.readLine().trim().split("\t");
-                String[] id = string[0].trim().split("-");
-                nuclide.setCategory(Nuclide.Category.valueOf(id[0]));
-                String name = id[1];
-                String num = id[2];
-                nuclide.setName(name);
-                nuclide.setNumStr(num);
-                nuclide.setState(UNIDENTIFIED);//todo добавил
-                int linesNum = Integer.parseInt(string[1]);
-                nuclide.setLinesNum(linesNum);
+                String firstLine = reader.readLine();
+                int nuclNum = Integer.parseInt(firstLine);
+                for (int i = 0; i < nuclNum; i++) {
+                    Nuclide nuclide = new Nuclide();
+                    String[] string = reader.readLine().trim().split("\t");
+                    String[] id = string[0].trim().split("-");
+                    nuclide.setCategory(Nuclide.Category.valueOf(id[0]));
+                    String name = id[1];
+                    String num = id[2];
+                    nuclide.setName(name);
+                    nuclide.setNumStr(num);
+                    nuclide.setState(UNIDENTIFIED);//todo добавил
+                    int linesNum = Integer.parseInt(string[1]);
+                    nuclide.setLinesNum(linesNum);
 
-                EnergyLine[] lines = new EnergyLine[linesNum];
-                int appender = 0;
-                for (int j = 0; j < linesNum; j++) {
-                    lines[j] = new EnergyLine(
-                            Float.parseFloat(string[2 + appender]),
-                            Integer.parseInt(string[3 + appender]),
-                            Integer.parseInt(string[4 + appender]),
-                            Integer.parseInt(string[5 + appender]),
-                            Integer.parseInt(string[6 + appender]));
-                    appender += 5;
+                    EnergyLine[] lines = new EnergyLine[linesNum];
+                    int appender = 0;
+                    for (int j = 0; j < linesNum; j++) {
+                        lines[j] = new EnergyLine(
+                                Float.parseFloat(string[2 + appender]),
+                                Integer.parseInt(string[3 + appender]),
+                                Integer.parseInt(string[4 + appender]),
+                                Integer.parseInt(string[5 + appender]),
+                                Integer.parseInt(string[6 + appender]));
+                        appender += 5;
+                    }
+                    nuclide.setEnergyLines(lines);
+                    list.add(nuclide);
                 }
-                nuclide.setEnergyLines(lines);
-                list.add(nuclide);
-            }
                 // этот стринг добавлен для: если будет не та кодировка, list.get(0).getName()
                 // не сможет нормально прочитать имя и выкинет эксепшн и сработает следующий за
                 // строкой кэтч. Так будет загружаться библиотека только в правильной кодировке
@@ -75,7 +100,7 @@ public final class NuclideLibraryReader {
                 Log.e(TAG, "       кодировка " + charset.displayName() + " не подходит");
             }
         }
-        return list;
+    return list;
     }
 
 }
